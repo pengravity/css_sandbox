aa43e5e67f7fd05a8737ed89bf9f5416a55f54a1
Andy <alipinski@mail.com>
2022-11-26 11:08:03 +0100

    opening and closing modals

--- css_sandbox/css_guide_pricing_plan/shared.js
+++ css_sandbox/css_guide_pricing_plan/shared.js
@@ -1,10 +1,30 @@
 const backdrop = document.querySelector('.backdrop');
 const modal = document.querySelector('.modal');
+const modalNoButton = document.querySelector('.modal__action-negative');
 const selectPlanButtons = document.querySelectorAll('.plan button');
-console.log(selectPlanButtons);
+const toggleButton = document.querySelector('.toggle-button');
+const mobileNav = document.querySelector('.mobile-nav');
 
 for (let i = 0; i < selectPlanButtons.length; i++) {
   selectPlanButtons[i].addEventListener('click', function () {
     modal.style.display = 'block';
+    backdrop.style.display = 'block';
   });
 }
+
+backdrop.addEventListener('click', function () {
+  mobileNav.style.display = 'none';
+  closeModal();
+});
+
+modalNoButton.addEventListener('click', closeModal);
+
+function closeModal() {
+  backdrop.style.display = 'none';
+  modal.style.display = 'none';
+}
+
+toggleButton.addEventListener('click', function () {
+  mobileNav.style.display = 'block';
+  backdrop.style.display = 'block';
+});
