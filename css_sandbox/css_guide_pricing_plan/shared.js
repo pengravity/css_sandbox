79f098c86c2db17b52ba36d48a3d1095c982b23a
Andy <alipinski@mail.com>
2022-11-26 13:03:33 +0100

    modal refactor - adding and removing classes, adding modals to customers and packages pages

--- css_sandbox/css_guide_pricing_plan/shared.js
+++ css_sandbox/css_guide_pricing_plan/shared.js
@@ -7,24 +7,36 @@ const mobileNav = document.querySelector('.mobile-nav');
 
 for (let i = 0; i < selectPlanButtons.length; i++) {
   selectPlanButtons[i].addEventListener('click', function () {
-    modal.style.display = 'block';
-    backdrop.style.display = 'block';
+    // modal.style.display = 'block';
+    // backdrop.style.display = 'block';
+    // modal.className = 'open'; // will overwerite classes list with just this one
+    modal.classList.add('open');
+    backdrop.classList.add('open');
   });
 }
 
 backdrop.addEventListener('click', function () {
-  mobileNav.style.display = 'none';
+  //   mobileNav.style.display = 'none';
+  mobileNav.classList.remove('open');
   closeModal();
 });
 
-modalNoButton.addEventListener('click', closeModal);
+if (modalNoButton) {
+  modalNoButton.addEventListener('click', closeModal);
+}
 
 function closeModal() {
-  backdrop.style.display = 'none';
-  modal.style.display = 'none';
+  //   backdrop.style.display = 'none';
+  //   modal.style.display = 'none';
+  if (modal) {
+    modal.classList.remove('open');
+  }
+  backdrop.classList.remove('open');
 }
 
 toggleButton.addEventListener('click', function () {
-  mobileNav.style.display = 'block';
-  backdrop.style.display = 'block';
+  //   mobileNav.style.display = 'block';
+  //   backdrop.style.display = 'block';
+  mobileNav.classList.add('open');
+  backdrop.classList.add('open');
 });
